aa03cb7b9f2afbe481db9489960f337d2165d77e
-- <---->
2023-10-10 01:08:19 +0300

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Татьяна Магнушевская on 10.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
